9b99f7693a01b5c87a7b12af7e371e8599839aa3
vaidehidangi23 <62616333+vaidehidangi23@users.noreply.github.com>
2022-11-09 17:00:11 +0530

    create and export our own modules

---
+++ importExport/index.js
@@ -0,0 +1,9 @@
+//const operator= require('./operator');
+//console.log(operator);
+//console.log(operator.add(5,5));
+//console.log(operator.mul(5,5));
+
+const {add,mul,a}= require('./operator');
+console.log(add(5,5));
+console.log(mul(5,5));
+console.log(a)
---
+++ importExport/operator.js
@@ -0,0 +1,12 @@
+const add = (a,b) =>{
+    return a+b;
+};
+const mul =(a,b) =>{
+    return a*b;
+}
+const a = 'vaidehi'
+
+//module.exports.add = add;
+//module.exports.mul = mul;
+
+module.exports = {add,a,mul};
