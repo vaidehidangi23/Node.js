cec5ff6d982f492f1c0069f83410aa7ec48104b1
vaidehidangi23 <62616333+vaidehidangi23@users.noreply.github.com>
2022-11-10 23:54:05 +0530

    Stream pipe

--- streamModule/index.js
+++ streamModule/index.js
@@ -1,21 +1,25 @@
 const fs = require('fs');
 const http = require('http');
+const { Stream } = require('stream');
 
 const server = http.createServer();
 
 server.on('request',(req,res)=>{
 const rstream = fs.createReadStream('input.txt');
+//1st way
+//rstream.on('data',(chunkdata)=>{
+//    res.write(chunkdata);
+//});
+//rstream.on('end',() => {
+//    res.end();
+//});
+//rstream.on('error',(err) => {
+//    console.log(err);
+//    res.end('file not found');
+//});
 
-rstream.on('data',(chunkdata)=>{
-    res.write(chunkdata);
-});
-rstream.on('end',() => {
-    res.end();
-});
-rstream.on('error',(err) => {
-    console.log(err);
-    res.end('file not found');
-});
+//2nd way
+rstream.pipe(res);
 });
 
 server.listen(8000, '127.0.0.1' );
